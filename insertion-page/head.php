32025c11f676118bcb45963de71b48ae718e0517
AwossiChrist <awossichrist@gmail.com>
2024-09-09 06:22:27 +0000

    SIte-Afpe

---
+++ insertion-page/head.php
@@ -0,0 +1,15 @@
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Afpesarl.com</title>
+    <link rel="stylesheet" href="/Fichier principal/style.css">
+    <link rel="stylesheet" href="/Fichier principal/responsive.css">
+    <link rel="shortcut icon" href="./image/afpe.png"/>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.css" rel="stylesheet">
+   
+</head>
+
+
+
---
+++ insertion-page/nav.php
@@ -0,0 +1,24 @@
+
+
+<nav class="nav1">
+
+<a class="" href=""><img src="/Fichier principal/logoafpesarl.png" alt=""></a>
+
+<div class="menu-icon" onclick="toggleMenu()">
+  <div class="bar"></div>
+  <div class="bar"></div>
+  <div class="bar"></div>
+</div>
+
+
+<div class="btns">
+    <button class="btn"><a href="index.html">Home</a></button>
+    <button class="btn"><a href="/A propos de nous/a propos.html" target="_blank">About</a></button>
+    <button class="btn"><a href="/services site/service.html" target="_blank">Services</a></button>
+    <button class="btn"><a href="/Gallery/gallery.html"target="_blank">Gallery</a></button>
+    <button class="btn"><a href="#">Updates</a></button>
+    <button class="btn"><a href="/form de contact/contact.html"target="_blank">Contact</a></button>
+    <button class="btn"><a href="/login/login.html"target="_blank">Login</a></button>
+</div>
+
+</nav>
